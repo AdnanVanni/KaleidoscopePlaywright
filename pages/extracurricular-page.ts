5289e4182b2c5ac2251b4958932e3cbb4fb606c4
Adnan <adnanvanni@gmail.com>
2025-04-10 15:08:57 +0530

    test

--- pages/extracurricular-page.ts
+++ pages/extracurricular-page.ts
@@ -46,7 +46,8 @@ export class ExtracurricularPage extends BasePage {
         for (const activity of activities) {
             await this.addExtracurricularActivity(activity);
         }
-        this.page.waitForTimeout(6000)
+
+        await this.nextPageButton.click();
         await this.nextPageButton.click();
         this.page.waitForTimeout(3000)
     }
